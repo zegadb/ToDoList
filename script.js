a92558ce8d08aaeeeb0a1c13f059fa56fd849533
zegadb <zeygammehtiev@gmail.com>
2022-11-03 19:18:26 +0400

    design part done 100%

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let count = 0
-
-document.querySelector('.add-button').addEventListener('click', () => { console.log(count); count++;})
-
 document.querySelector('.sort').addEventListener('click', (event) =>
 {
     if (event.target.src.indexOf("images/downgray.svg") != -1) event.target.src = 'images/upblack.svg';
     else event.target.src = 'images/downgray.svg';
 })
 
-document.querySelector('.delete').addEventListener('click', (event) =>
+document.querySelectorAll('.delete').forEach(item => { item.addEventListener('mouseover', (event) =>
+{ event.target.src = 'images/deleteX.svg' })})
+document.querySelectorAll('.delete').forEach(item => { item.addEventListener('mouseout', (event) =>
+{ event.target.src = 'images/delete.svg' })})
