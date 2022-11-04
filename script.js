c4557b3fdcf0e8c379d43f7a4e87e538797a04d4
zegadb <zeygammehtiev@gmail.com>
2022-11-04 18:51:45 +0400

    done the sort button darkening effect on hover and other small changes

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+// Sorting confirmed lines
 function sortList(order)
 {
     let inputs = [];
     document.querySelectorAll('.line')
-    .forEach(item => inputs.push(item.querySelector('input').value))
+    .forEach(item => inputs.push(item.textContent))
     if (order) inputs.sort();
     else inputs.sort().reverse();
     document.querySelectorAll('.line')
-    .forEach((item, index) => item.querySelector('input').value = inputs[index])
+    .forEach((item, index) => item.textContent = inputs[index])
 }
-
+// Sort button listener
 document.querySelector('.sort').addEventListener('click', (event) =>
 {
     if (event.target.src.indexOf("images/downgray.svg") != -1)
-    { event.target.src = 'images/upblack.svg'; sortList(true) }
+    { event.target.src = 'images/upgray.svg'; sortList(true) }
     else { event.target.src = 'images/downgray.svg'; sortList(false); }
 })
+// Replaces inputs with text lines
+function confirmInput(event)
+{
+    let newP = document.createElement('div')
+    newP.className = 'text'
+    newP.textContent = event.target.value
+    // console.log(newP)
+    // console.log(event.target.parentElement.firstElementChild)
+    event.target.parentElement.insertBefore(newP, event.target.parentElement.firstElementChild)
+    event.target.parentElement.querySelector('input').remove()
 
+    document.querySelectorAll('.delete').forEach(item => { item.addEventListener('click', (event) => { if (event.target.parentElement.firstElementChild.className == 'text') event.target.parentElement.parentElement.remove()})})
+}
+// Updates event listeners for appended elements
 function update()
 {
     document.querySelectorAll('.delete').forEach(item => { item.addEventListener('mouseover', (event) =>
@@ -24,29 +38,19 @@ function update()
     { event.target.src = 'images/delete.svg' })})
     document.querySelectorAll('.delete').forEach(item => { item.addEventListener('click', (event) => { event.target.parentElement.parentElement.querySelector('input').value = '' })})
     const dragArea = document.querySelector('.input-area')
-    new Sortable(dragArea, {animation: 350})
+    new Sortable(dragArea, {animation: 300})
     document.querySelector('input').addEventListener('keyup', (event) =>
     {
-        if (event.key == 'Enter' && event.target.value != '' && event.target.value != ' ') {
-            let newP = document.createElement('div')
-            newP.className = 'text'
-            newP.textContent = event.target.value
-            console.log(newP)
-            console.log(event.target.parentElement.firstElementChild)
-            event.target.parentElement.insertBefore(newP, event.target.parentElement.firstElementChild)
-            event.target.parentElement.querySelector('input').remove()
-        }
-        document.querySelectorAll('.delete').forEach(item => { item.addEventListener('click', (event) => { if (event.target.parentElement.firstElementChild.className == 'text') event.target.parentElement.parentElement.remove()})})
+        if (event.key == 'Enter' && event.target.value != '' && event.target.value != ' ')
+            confirmInput(event);
     })
-}
-update()
-
+} update()
+// Add button listener
 document.querySelector('.add-button').addEventListener('click', () =>
 {
     let newElement = document.createElement('div')
-    let scroll = document.createElement('a')
     newElement.innerHTML = '<div class="line">\n<input type="text">\n<button class="delete"><img src="images/delete.svg" alt="img"></button>\n</div>'
     document.querySelector('.input-area').append(newElement)
-    document.querySelector('.input-area').lastElementChild.scrollIntoView({behavior: "smooth"})
+    document.querySelector('.input-area').lastElementChild.scrollIntoView({behavior: "smooth"}) 
     update()
 })
