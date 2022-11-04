331dfcac19643e6ff57b6c59183fba25799a0333
zegadb <zeygammehtiev@gmail.com>
2022-11-04 19:25:31 +0400

    added some comments for easy navigation and other improvements

--- script.js
+++ script.js
@@ -29,21 +29,29 @@ function confirmInput(event)
 
     document.querySelectorAll('.delete').forEach(item => { item.addEventListener('click', (event) => { if (event.target.parentElement.firstElementChild.className == 'text') event.target.parentElement.parentElement.remove()})})
 }
+// Removes or clears the line
+function clearLine(event)
+{
+    if (event.target.parentElement.parentElement.firstElementChild.tagName == 'INPUT')
+        event.target.parentElement.parentElement.querySelector('input').value = '';
+    else event.target.parentElement.parentElement.remove();
+}
 // Updates event listeners for appended elements
 function update()
 {
+    // Delete button hover effect
     document.querySelectorAll('.delete').forEach(item => { item.addEventListener('mouseover', (event) =>
     { event.target.src = 'images/deleteX.svg' })})
     document.querySelectorAll('.delete').forEach(item => { item.addEventListener('mouseout', (event) =>
     { event.target.src = 'images/delete.svg' })})
-    document.querySelectorAll('.delete').forEach(item => { item.addEventListener('click', (event) => { event.target.parentElement.parentElement.querySelector('input').value = '' })})
+    // Delete button listener
+    document.querySelectorAll('.delete').forEach(item => item.addEventListener('click', clearLine))
+    // Draggable
     const dragArea = document.querySelector('.input-area')
     new Sortable(dragArea, {animation: 300})
+    // Confirm input and turn it into text line
     document.querySelector('input').addEventListener('keyup', (event) =>
-    {
-        if (event.key == 'Enter' && event.target.value != '' && event.target.value != ' ')
-            confirmInput(event);
-    })
+    { if (event.key == 'Enter' && event.target.value != '' && event.target.value != ' ') confirmInput(event); })
 } update()
 // Add button listener
 document.querySelector('.add-button').addEventListener('click', () =>
